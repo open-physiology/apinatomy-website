bab063b2d6de161ea6df05864c0867bdbf2929bd
mhelvens <mhelvens@gmail.com>
2014-09-07 14:29:40 +0200

    Added content to the development page and fixed some style issues.

--- client/development/controller.js
+++ client/development/controller.js
@@ -1,10 +1,6 @@
-define(['/site/module.js'], function (site) {
+define(['/site/module.js', 'css!/development/style'], function (site) {
 
     site.controller('DevelopmentController', function DevelopmentController($scope) {
-
-
-
-
 
     });
 
